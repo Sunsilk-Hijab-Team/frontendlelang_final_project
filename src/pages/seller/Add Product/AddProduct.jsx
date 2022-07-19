cbdca7b089f7577b453ca5e85be26651c4f9a118
FSW3-CH3-Muhammad Mufid Ridho <muhammadmufidridho77@gmail.com>
2022-07-19 14:20:00 +0700

    add product : failed

--- src/pages/seller/Add Product/AddProduct.jsx
+++ src/pages/seller/Add Product/AddProduct.jsx
@@ -1,9 +1,7 @@
 import Navbar from '../../../components/NavbarAfterLogin/NavbarDashboard';
-import React, {Component, useState, useEffect} from 'react';
+import React, { Component } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-// import Previous from './fi_arrow-left.svg';
 import './styleAddProduct.css';
-
 import Image_1 from './Group_1.png';
 import Image_2 from './Group_86.png';
 import styleRegister from '../../../components/Register/register.module.css';
@@ -16,7 +14,7 @@ export default class addProduct extends Component {
         super(props);
         this.onChangeProductname = this.onChangeProductname.bind(this);
         this.onChangePrice = this.onChangePrice.bind(this);
-        this.onChangeCategory = this.onChangeCategory.bind(this);
+        this.onChangeCategory = this.onChaneCategory.bind(this);
         this.onChangeDescription = this.onChangeDescription.bind(this);
         this.saveProduct = this.saveProduct.bind(this);
         this.newProduct = this.newProduct.bind(this);
@@ -60,7 +58,6 @@ export default class addProduct extends Component {
             price: this.state.price,
             category: this.state.category,
             description: this.state.description,
-            published: Response.data.published,
             submitted: true,
         };
 
@@ -104,90 +101,115 @@ export default class addProduct extends Component {
                     {this.state.submitted ? (
                         <div>
                             <h4>Produkmu telah ditambahkan</h4>
-                            <button className="btn btn-success" onClick={this.newTutorial}>
-                                Tambahakan lainya
+                            <button
+                                className="btn btn-success" onClick={this.newProduct}>
+                                Tambahkan lainya
                             </button>
                         </div>
                     ) : (
                         <div>
                             <Row>
-                        <Col sm={12}>
-                            <Form className={styleRegister.formStyle}>
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label className='add-product-label'>
-                                        Product Name
-                                    </Form.Label>
-                                    <Form.Control className={styleRegister.rounded} type="text" placeholder="Product Name"
-                                    id="productname"
-                                    required
-                                    valeu={this.state.productname}
-                                    onChange={this.onChangeProductname}
-                                    />
-                                </Form.Group>
-    
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label className='add-product-label'>Price</Form.Label>
-                                    <Form.Control className={styleRegister.rounded} type="email" placeholder="Rp 0,00" />
-                                </Form.Group>
-    
-                                <Form.Group className="
-                                mb-3" controlId="formBasicEmail">
-                                    <Form.Label className='add-product-label'>Category</Form.Label>
-                                    <Form.Control className={styleRegister.rounded} type="email" placeholder="Choose Category" 
-                                    id="category"
-                                    required
-                                    value= {this.state.category}
-                                    onChange ={this.onChangeCategory}
-                                    />
-                                </Form.Group>
-    
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label className='add-product-label'>Product Description</Form.Label>
-                                    <Form.Control className={styleRegister.rounded} type="email" placeholder="ex: Jalan Ikan Hiu 33" 
-                                    id="description"
-                                    reuqired
-                                    valeu={this.state.description}
-                                    onChange={this.onChangeDescription}
-                                    />
-                                </Form.Group>
-    
-                                {/* <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Col sm={12}>
+                                    <Form className={styleRegister.formStyle}>
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label className='add-product-label'>
+                                                Product Name
+                                            </Form.Label>
+                                            <Form.Control
+                                                className={styleRegister.rounded}
+                                                type="text"
+                                                placeholder="Product Name"
+                                                id="productname"
+                                                required
+                                                valeu={this.state.productname}
+                                                onChange={this.onChangeProductname}
+                                                name="productname"
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label className='add-product-label'>Price</Form.Label>
+                                            <Form.Control
+                                                className={styleRegister.rounded}
+                                                type="text"
+                                                placeholder="Rp 0,00"
+                                                id="price"
+                                                required
+                                                value={this.state.price}
+                                                onChange={this.onChangePrice}
+                                                name="price"
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label className='add-product-label'>Category</Form.Label>
+                                            <Form.Control
+                                                className={styleRegister.rounded}
+                                                type="text"
+                                                placeholder="Choose Category"
+                                                id="category"
+                                                required
+                                                value={this.state.category}
+                                                onChange={this.onChangeCategory}
+                                                name="category"
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label className='add-product-label'>Product Description</Form.Label>
+                                            <Form.Control
+                                                className={styleRegister.rounded}
+                                                type="text"
+                                                placeholder="ex: Jalan Ikan Hiu 33"
+                                                id="description"
+                                                required
+                                                value={this.state.description}
+                                                onChange={this.onChangeDescription}
+                                                name="description"
+                                            />
+                                        </Form.Group>
+
+                                        {/* <Form.Group className="mb-3" controlId="formBasicPassword">
                                         <Form.Label className='add-product-label'>Category</Form.Label>
                                         <span className="icons-span" onClick={handelToggle}>
                                             {passwordIcon}
                                         </span>
                                         <Form.Control className={styleRegister.rounded} placeholder="min 6 characters" type={passwordType} />
                                     </Form.Group> */}
-                            </Form>
-                        </Col>
-    
-                        <Col sm={12} className='photo d-flex flex-column align-content-center'>
-                            <img className='image_1' src={Image_1} alt="" />
-                            <p className='add-photo-label'>Product Photo</p>
-                            <div className='d-flex additional'>
-                                <img className='image_2' src={Image_2} alt="" />
-                                <img className='d-flex' src={Image_2} alt="" />
-                            </div>
-                            <p className='add-photo-label-more'>Add More</p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <div className='button-add-product'>
-                                <Button className='styleButtonPreview' type="submit">
-                                    Preview
-                                </Button>
-                                <span></span>
-                                <Button className='styleButton' variant="primary" type="submit">
-                                    Post
-                                </Button>
-                            </div>
-                        </Col>
-                        <Col>
-                        </Col>
-                    </Row>
- 
-                        </div>                   )}
+                                    </Form>
+                                </Col>
+
+                                <Col sm={12} className='photo d-flex flex-column align-content-center'>
+                                    <img className='image_1' src={Image_1} alt="" />
+                                    <p className='add-photo-label'>Product Photo</p>
+                                    <div className='d-flex additional'>
+                                        <img className='image_2' src={Image_2} alt="" />
+                                        <img className='d-flex' src={Image_2} alt="" />
+                                    </div>
+                                    <p className='add-photo-label-more'>Add More</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <div className='button-add-product'>
+                                        <Button className='styleButtonPreview' type="submit">
+                                            Preview
+                                        </Button>
+                                        <span></span>
+                                        <Button
+                                        className='styleButton'
+                                        variant="primary"
+                                        type="submit"
+                                        onClick={this.saveProduct}
+                                        >
+                                            Post
+                                        </Button>
+                                    </div>
+                                </Col>
+                                <Col>
+                                </Col>
+                            </Row>
+                        </div>)}
                 </Container>
             </div>
         )
