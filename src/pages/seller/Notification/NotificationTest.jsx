0058e9a55af1d5c9cb205e4fb3e47c3ef8beb1e1
triyasniko <triyasniko@gmail.com>
2022-07-19 18:12:50 +0700

    update notif test

--- src/pages/seller/Notification/NotificationTest.jsx
+++ src/pages/seller/Notification/NotificationTest.jsx
@@ -1,58 +1,65 @@
 import {React, useEffect, useState} from 'react';
 import {Col, Container, Row } from 'react-bootstrap';
-import io from "socket.io-client";
+// import axios
+import axios from 'axios';
+// import io from "socket.io-client";
 
 function NotificationTest() {
-    const token=localStorage.setItem("token", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJlbWFpbCI6InRyaXlhc25pa29AZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmEkMTAkRjl5OE5ReVJYeFF3cmJPWldOSmwxT2hLTUMwRUVtRktNMVN3dldRWEYxenU0ck0zUi52bGUiLCJwaG9uZSI6bnVsbCwiY2l0eSI6bnVsbCwiYWRkcmVzcyI6bnVsbCwiaW1hZ2VfdXJsIjpudWxsfSwiaWF0IjoxNjU3ODkxNDMxfQ.95dyCzazXpEmSxbt0rNRLNbnDV6oaNjAQhPIPSRWZjA");
-    // const [notification, setNotification]=useState("");
-    const [notificationList, setNotificationList]=useState([]);
+    const jwt="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmdWxsX25hbWUiOiJKb2huIERvZWVlZWUiLCJlbWFpbCI6InRyaXlhc25pa28yQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiMTIzIiwiY2l0eSI6bnVsbCwiYWRkcmVzcyI6bnVsbCwiaW1hZ2VfdXJsIjpudWxsfSwiaWF0IjoxNjU4MTQxNjc3fQ.8a2ljyJ7hrliiRTKpZj5_akgBKfVO7FjckKlmMSk5fo";
+    const token=localStorage.setItem("token", "Bearer "+jwt);
+    const [notification, setNotification]=useState([]);
     // get item token from localstorage
     const tokenItem=localStorage.getItem("token");
+    const REACT_API_URL="https://lelang-euyy.herokuapp.com";
 
-    useEffect(()=>{
-        fetch("http://localhost:8000/api/v1/notification/all", {
-            method: "GET",
-            headers: {
-                "Authorization": tokenItem,
-                "Content-Type": "application/json"
+    const getAllNotification=async()=>{
+        const response=await axios.get(`${REACT_API_URL}/api/v1/notification/all`,{
+            headers:{
+                Authorization:tokenItem
             }
         })
-        .then(res=>{
-            // console.log("res oiii"+res.json());
-            return res.json();
+        .then((response)=>{
+            // console.log(response.data);
+            const allNotification= response.data.Notifications;
+            // console.log(allNotification);
+            setNotification(allNotification);
+            // console.log(notification);
         })
-        .then(data=>{
-            console.log("data oiii"+data.data.Notifications);
-            // setNotificationList(data.data.Notifications);
+        .catch((error)=>{
+            console.log(error);
         })
-        .catch(err=>{
-            console.log(err);
-        })
-    }, []);
-    // console.log user id from user login
-    console.log(
-        
-    )
+    }
 
+    useEffect(()=>{
+        getAllNotification();
+    },[]);
+    
+    // console.log("########"+notification);
     return (
         <div>
-            Notification Test
             {/* <Navbar title="Notification" />
             <Container className='previous'>
                 <PreviousButton />
             </Container> */}
 
-            {/* <Container>
+            <Container>
                 <Row>
                     <Col md={12} >
-                        {notificationList.map((notification, index) => (
-                            <div key={index}>
-                                {notification}
-                            </div>
-                        ))}
+                        <h1>Notification</h1>
+                        {/* {console.log("#############"+notification.Notifications)} */}
+                        {/* looping notification data */}
+                        {notification.map((item, index)=>{
+                            console.log(item);
+                            return(
+                                <div key={index}>
+                                    <h3>{item.read_status}</h3>
+                                    <p>{item.transaction_date}</p>
+                                </div>
+                            )
+                        })}
                     </Col>
                 </Row>
-            </Container> */}
+            </Container>
         </div >
 
     );
