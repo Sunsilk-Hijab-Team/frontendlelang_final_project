309867b5e991b45ebc935a224ba83fac08983c96
ALI FAHRIAL ANWAR <alifahrialanwar@gmail.com>
2022-07-19 13:47:03 +0700

    Update ButtonLogout, and fixed isLogin or Not

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 // import NavigationBar from '../../components/NavbarDashboardBuyer/NavbarDashboard';
 import NavigationBar from '../../components/NavbarAfterLogin/NavbarDashboard';
+import NavigationBarBefore from '../../components/NavbarBeforeLogin/NavbarDashboard';
 import Carousel from '../../components/Carousel/Carousel';
 import CardComponent from '../../components/Cards/CardProduct';
 import ButtonFilter from '../../components/buttonFilter/ButtonFilter';
@@ -7,20 +8,39 @@ import React from 'react';
 import './styleHome.css';
 import ButtonLogin from '../../components/ButtonLogin/ButtonLogin';
 import Search from '../../components/Search/Search';
+import SellButton from '../../components/SellButton/SellButton';
 
 
 function Home() {
 
+    const token = localStorage.getItem('token');
+
     return (
         <div>
-            <NavigationBar
-            middle={<Search/>}
-            right={<ButtonLogin/>}
-            />
+            {
+                token ?
+                 <NavigationBar
+                    middle={<Search/>}
+                    />
+
+                 :
+
+                <NavigationBarBefore
+                middle={<Search/>}
+                right={<ButtonLogin/>}
+                />
+
+            }
+
             <Carousel />
             <ButtonFilter />
             <CardComponent
             />
+
+            {
+                token ? <SellButton /> : <></>
+            }
+
         </div>
     )
 }
