d3ee6de71798c3f2303a83f50f9c00a7103978ba
ALI FAHRIAL ANWAR <alifahrialanwar@gmail.com>
2022-07-23 03:21:49 +0700

    Updated

--- src/pages/seller/PreviewProduct/PreviewProduct.jsx
+++ src/pages/seller/PreviewProduct/PreviewProduct.jsx
@@ -3,7 +3,7 @@ import { Col, Container, Row, Button, Card, Spinner, Form } from 'react-bootstra
 import Carousel from 'react-bootstrap/Carousel';
 import NoImage from '../../../images/no_image.png';
 import Style from './stylePreviews.module.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { React, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Rupiah} from '../../../components/CostumFunction/Rupiah';
@@ -16,6 +16,7 @@ function PreviewProduct() {
 
     const url = REACT_APP_API_URL;
     let { productId } = useParams();
+    let nav = useNavigate();
     const token = localStorage.getItem('token');
     const [loading, setLoading] = useState(false);
     const [load, setLoad] = useState(false);
@@ -23,7 +24,6 @@ function PreviewProduct() {
     const [images, setImages] = useState([]);
     const [categories, setCategories] = useState([]);
     const [user, setUser] = useState([]);
-    const [published, setPublished] = useState('');
 
     const getDetail = async () => {
         setLoading(true)
@@ -51,9 +51,7 @@ function PreviewProduct() {
     }
 
     const goPublish = async (e) => {
-        console.log('sadas')
         let published = true;
-        console.log(published, '-----bol-')
         // e.prevetDefault()
         try {
             // await axios.put(`${url}/api/v1/seller/product/update/${productId}`, published,{
@@ -83,6 +81,10 @@ function PreviewProduct() {
             console.log(error.message)
             setLoad(false)
         }
+    }
+
+    const goUpdate = async (e) => {
+        nav(`/seller/product/update/${productId}`)
     }
 
     useEffect(() => {
@@ -166,7 +168,7 @@ function PreviewProduct() {
                                     </Button>
                             }
                             {/* button outline danger */}
-                            <Button className={Style.buttonEdit}>
+                            <Button onClick={goUpdate} type="button" className={Style.buttonEdit}>
                                 Edit
                             </Button>
                         </Card>
