e90b267b6ab84731c30ab2aebfcef06e33bee546
triyasniko <triyasniko@gmail.com>
2022-07-24 14:32:34 +0700

    back before merge

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -4,42 +4,39 @@ import Style from './styleDetails.module.css';
 import Carousel from 'react-bootstrap/Carousel';
 import NoImage from '../../images/no_image.png';
 import PreviousButton from '../PreviousButton/PreviousButton';
-import './styleDetails.module.css';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import PopUp from './popup/PopUp';
-import Login from '../ButtonLogin/ButtonLogin';
-import Navbar from '../NavbarBeforeLogin/NavbarDashboard';
 
 const { REACT_APP_API_URL } = process.env
 
-function SellerHome() {
+function  SellerHome() {
 
     const url = `${REACT_APP_API_URL}/api/v1/buyer/product/`;
     let { productId } = useParams();
     const [item, setItem] = useState([]);
-    const [category, setCategory] = useState([]);
+    const [category, setCategory]  = useState([]);
     const [images, setImages] = useState([]);
     const [loading, setLoading] = useState(false);
 
     const Detail = async () => {
         setLoading(true)
         try {
-            await axios.get(url + productId)
-                .then(res => {
-                    setItem(res.data.data.product);
-                    setCategory(res.data.data.product.categories);
-                    setImages(res.data.data.product.images);
-                    // console.log(images, 'null')
-                })
+            await axios.get(url+productId)
+            .then(res => {
+                setItem(res.data.data.product);
+                setCategory(res.data.data.product.categories);
+                setImages(res.data.data.product.images);
+                // console.log(images, 'null')
+            })
             setLoading(false)
         } catch (error) {
             setLoading(true);
         }
     }
-    console.log("itemsss", item);
+
     useEffect(() => {
         Detail();
     }, [])
@@ -57,40 +54,10 @@ function SellerHome() {
                                 <span className="visually-hidden">Loading...</span>
                             </Spinner>
                         </Row>
-                        : <></>
+                    : <></>
                 }
                 <Row>
                     <Col>
-                        <Carousel className={Style.carousel}>
-                            {
-                                images.length === 0 ?
-
-                                    <Carousel.Item className={Style.carousel}>
-                                        <img className={Style.carouselImage} src={NoImage} alt="productImage" />
-                                    </Carousel.Item>
-
-                                    :
-
-                                    images.map((image, index) => {
-                                        return (
-                                            <Carousel.Item key={index} className={Style.carousel}>
-                                                <img className={Style.carouselImage} src={image.image_url} alt="productImage" />
-                                            </Carousel.Item>
-                                        )
-                                    })
-                            }
-                        </Carousel>
-                    </Col>
-
-                    <Col className={Style.desc}>
-                        <div className={Style.sellerProfile}>
-                            <img className={Style.sellerPhoto} src={item.users.image_url?item.users.image_url: NoImage} alt="" />
-                            <div className='ms-3'>
-                                <h2 className={Style.sellerName}>{item.users?item.users.full_name:"Undefined"}</h2>
-                                <h1 className={Style.city}>{item.users?item.users.address:"Undefined"}</h1>
-                            </div>
-                        </div>
-
                         <h4 className={Style.h4}>{category === null ? 'Tidak Berkategori' : category.name}</h4>
                         <h1 className={Style.h1}>{item.name}</h1>
                         <div className='d-flex flex-row align-items-center'>
@@ -100,6 +67,28 @@ function SellerHome() {
                         <p className={Style.p}>{item.description}</p>
 
                         <PopUp />
+                    </Col>
+
+                    <Col>
+                        <Carousel className={Style.carousel}>
+                                {
+                                    images.length === 0 ?
+
+                                    <Carousel.Item className={Style.carousel}>
+                                        <img className={Style.imgcarousel} src={NoImage} alt="productImage" />
+                                    </Carousel.Item>
+
+                                    :
+
+                                    images.map((image, index) => {
+                                        return (
+                                            <Carousel.Item key={index} className={Style.carousel}>
+                                                <img className={Style.imgcarousel} src={image.image_url} alt="productImage" />
+                                            </Carousel.Item>
+                                        )
+                                    })
+                                }
+                        </Carousel>
                     </Col>
                 </Row>
             </Container>
